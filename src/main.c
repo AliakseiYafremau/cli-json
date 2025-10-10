a004612ba5b791ede8fb86e55c63ada07228469c
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-10-10 02:27:07 +0100

    Upgrade cli

--- src/main.c
+++ src/main.c
@@ -8,31 +8,55 @@
 int parse_arguments(int argc, char* argv[]);
 
 int main(int argc, char* argv[]) {
-    printf("argc = %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-
     int result = parse_arguments(argc, argv);
-
     return result;
 }
 
 int parse_arguments(int argc, char* argv[]) {
     if (argc == 1) {
-        printf("Параметры не введены\n");
+        printf("Parameters not specified. For help, try 'cli-json help'.\n");
         return 1;
-    } else if (!(strcmp(argv[1], "read"))) {
-        char* file_content_pointer = read_file("example.json");
-        printf("%s\n", file_content_pointer);
-        free(file_content_pointer);
-    } else if (!(strcmp(argv[1], "format"))) {
-        char* file_content_pointer = read_file("example.json");
-        char* formated = format_json(file_content_pointer);
-        printf("%s\n", formated);
-        free(file_content_pointer);
-        free(formated);
     }
+    if (!(strcmp(argv[1], "help"))) {
+        printf(
+            "CLI-json is a tool is designed for fast formatting of JSON "
+            "files.\n\n"
+            "Available commands:\n"
+            "\thelp                     print this message\n"
+            "\tread <filename>          output formatted content of the file\n"
+            "\tformat <filename>        format the file\n"
+        );
+        return 0;
+    }
+
+    if ((strcmp(argv[1], "read") && (strcmp(argv[1], "format")))) {
+        printf("Incorrect parameters. For help, try 'cli-json help'.\n");
+        return 1;
+    }
+
+    char* file_content_pointer = read_file(argv[2]);
+
+    if (!file_content_pointer) {
+        printf("Incorrect file path entered: '%s'\n", argv[2]);
+        return 1;
+    }
+    char* formated = format_json(file_content_pointer);
+    if (!(strcmp(argv[1], "read"))) {
+        printf("%s\n", file_content_pointer);
+    } else if (!(strcmp(argv[1], "format"))) {
+        FILE* file_to_format = fopen(argv[2], "w");
+        if (!file_to_format) {
+            printf("Не удалось открыть файл для записи: '%s')", argv[2]);
+            free(formated);
+            free(file_content_pointer);
+            return 1;
+        }
+        fprintf(file_to_format, "%s", formated);
+        fclose(file_to_format);
+    }
+
+    free(formated);
+    free(file_content_pointer);
 
     return 0;
 }
